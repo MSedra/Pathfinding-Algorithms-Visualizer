8c88e6bdf4a65c5bd5ef3023f9b2d49567c16efe
MSedra <62825437+MSedra@users.noreply.github.com>
2020-10-14 10:37:26 +0200

    initial commit

--- README.md
+++ README.md
@@ -5,18 +5,24 @@ Heuristic and Blind algorithms visualizer
 
 ## Notes 
 
-* First, choose the starting node "Colored green"
-* Then, choose the ending node "Colored blue"
+* First, choose the source node "Colored green"
+* Then, choose the destination node "Colored blue"
 * After that, you can choose any number of nodes to be blocks "Colored red" except the starting and ending nodes
 * Press
-  * 'A' to run A* algorithm
+  * 'A' to run A* 
   * 'T' to run Best First Search
-  * 'I' to run BiBfs
   * 'J' to run Dijkstra
   * 'B' to run BFS
+  * 'I' to run BiBfs
   * 'D' to run DFS
-* If a path is found, the nodes in the path will be colored yellow showing the exact path/shortest path
+*If there is a path from the source to the destination,
+  *After running DFS, the destination node will be colored yellow
+  *After running any other algorithm, all the nodes in the path will be colored yellow showing the exact shortest path
 * Wait until the current algorithm's visualization is finished
 * Then
   * If you want to run another algorithm on the same maze, press the letter of the wanted algorithm
   * press 'R' to reset the maze
+ *change sleepingTime constant variables accordingly to increase/decrease the speed of the visualization of an algorithm or backtracking the path
+** It should be noted that shortest path algorithms here can simply be replaced by BFS because
+** there is no weights, but the main purpose is to visualize the algorithms and get a better
+** understanding of how they work.
--- src/Main.cpp
+++ src/Main.cpp
@@ -8,18 +8,6 @@ using namespace std;
 	#define render_header
 void render(int);
 #endif
-#ifndef cmp_header
-	#define cmp_header
-bool cmp(pair<int, int>, pair<int, int>);
-#endif
-#ifndef cmp2_header
-	#define cmp2_header
-bool cmp2(pair<int, int>, pair<int, int>);
-#endif
-#ifndef cmp3_header
-	#define cmp3_header
-bool cmp3(pair<int, int>, pair<int, int>);
-#endif
 #ifndef calcH_header
 	#define calcH_header
 int calcH(int, int, int, int);
@@ -39,6 +27,10 @@ void reset(int, int, int, int);
 #ifndef clearYellow_header
 	#define clearYellow_header
 void clearYellow();
+#endif
+#ifndef dfs_header
+	#define dfs_header
+void dfs(int, int, int, int, int, int, bool&);
 #endif
 #ifndef bfs_header
 	#define bfs_header
@@ -61,15 +53,23 @@ void dijkstra(int, int, int, int);
 void aStar(int, int, int, int);
 #endif
 
-const int sz = 30, ver = 33, hor = 62, tot = 8;
+const int sz = 30, ver = 33, hor = 62, tot = 8; // ver -> height of the screen, hor -> width of the screen ,thus ver*hor nodes
 const int maxx = 5050;
 const int vx = sz * hor, vy = sz * ver;
+const int backtrackingSleepingTime = 8;
+const int dfsSleepingTime = 12;
+const int bfsSleepingTime = 3;
+const int bibfsSleepingTime = 5;
+const int bestFirstSearchSleepingTime = 12;
+const int dijkstraSleepingTime = 3;
+const int aStarSleepingTime = 9;x	
+// change sleepingTime to increase/decrease the speed of the visualization of an algorithm or backtracking the path
 
 sf::RenderWindow window(sf::VideoMode(vx, vy), "Algorithm Visualizer", sf::Style::Default);
 sf::RectangleShape player(sf::Vector2f(sz, sz));
 
-vector<pair<int, int>> dir = { { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 }, { -1, -1 }, { 1, 1 }, { 1, -1 }, { -1, 1 } };
-bool visited[ver + 10][hor + 10], visited2[ver + 10][hor + 10], open[ver + 10][hor + 10];
+vector<pair<int, int>> dir = { { 1, 0 }, { 0, 1 }, { 1, 1 }, { -1, 1 }, { 1, -1 }, { -1, 0 }, { -1, -1 }, { 0, -1 } };
+bool visited[ver + 10][hor + 10], visited2[ver + 10][hor + 10], exists[ver + 10][hor + 10];
 int col[ver + 10][hor + 10], pi[ver + 10][hor + 10], pj[ver + 10][hor + 10], pi2[ver + 10][hor + 10], pj2[ver + 10][hor + 10], dist[ver + 10][hor + 10], g[ver + 10][hor + 10], h[ver + 10][hor + 10];
 vector<pair<int, int>> openList;
 
@@ -101,23 +101,33 @@ void render(int sleepingTime)
 	Sleep(sleepingTime);
 	sleepingTime++;
 }
-
-bool cmp(pair<int, int> p1, pair<int, int> p2) // DIJKSTRA
+class cmp
 {
-	return dist[p1.first][p1.second] > dist[p2.first][p2.second];
-}
-
-bool cmp2(pair<int, int> p1, pair<int, int> p2) // A*
+public:
+	bool operator()(const pair<int, int>& p1, const pair<int, int>& p2)
+	{
+		return dist[p1.first][p1.second] > dist[p2.first][p2.second];
+	}
+};
+class cmp2
 {
-	int f1 = g[p1.first][p1.second] + h[p1.first][p1.second];
-	int f2 = g[p2.first][p2.second] + h[p2.first][p2.second];
-	return f1 > f2;
-}
+public:
+	bool operator()(const pair<int, int>& p1, const pair<int, int>& p2)
+	{
+		int f1 = g[p1.first][p1.second] + h[p1.first][p1.second];
+		int f2 = g[p2.first][p2.second] + h[p2.first][p2.second];
+		return f1 > f2;
+	}
+};
 
-bool cmp3(pair<int, int> p1, pair<int, int> p2) // BEST FIRST SEARCH
+class cmp3
 {
-	return h[p1.first][p1.second] > h[p2.first][p2.second];
-}
+public:
+	bool operator()(const pair<int, int>& p1, const pair<int, int>& p2)
+	{
+		return h[p1.first][p1.second] > h[p2.first][p2.second];
+	}
+};
 
 int calcH(int x1, int y1, int x2, int y2)
 {
@@ -135,14 +145,14 @@ void backTrack(int ei, int ej, bool bi)
 		ok2 = false;
 		pari2 = ei;
 		col[ei][ej] = 4;
-		render(5);
+		render(backtrackingSleepingTime);
 	}
 	while (true)
 	{
 		if (pari != -1)
 		{
 			col[pari][parj] = 4;
-			render(5);
+			render(backtrackingSleepingTime);
 			npari = pi[pari][parj], nparj = pj[pari][parj];
 			pari = npari, parj = nparj;
 		}
@@ -151,7 +161,7 @@ void backTrack(int ei, int ej, bool bi)
 		if (pari2 != -1)
 		{
 			col[pari2][parj2] = 4;
-			render(5);
+			render(backtrackingSleepingTime);
 			npari2 = pi2[pari2][parj2], nparj2 = pj2[pari2][parj2];
 			pari2 = npari2, parj2 = nparj2;
 		}
@@ -174,7 +184,7 @@ void reset(int si, int sj, int ei, int ej)
 {
 	memset(visited, false, sizeof(visited));
 	memset(visited2, false, sizeof(visited2));
-	memset(open, false, sizeof(open));
+	memset(exists, false, sizeof(exists));
 	memset(pi, -1, sizeof(pi));
 	memset(pj, -1, sizeof(pj));
 	memset(pi2, -1, sizeof(pi2));
@@ -197,6 +207,36 @@ void clearYellow()
 	render(0);
 }
 
+void dfs(int i, int j, int si, int sj, int ei, int ej, bool& done)
+{
+	visited[i][j] = true;
+	if (!((i == si && j == sj) || (i == ei && j == ej)))
+		col[i][j] = 1;
+	render(dfsSleepingTime);
+	if (i == ei && j == ej)
+	{
+		clear();
+		col[ei][ej] = 4;
+		done = true;
+		return;
+	}
+	for (int z = 0; z < 8; ++z)
+	{
+		int ni = i + dir[z].first;
+		int nj = j + dir[z].second;
+		if (ni >= 0 && ni < ver && nj >= 0 && nj < hor && col[ni][nj] != 3 && !visited[ni][nj])
+		{
+			pi[ni][nj] = i, pj[ni][nj] = j;
+			dfs(ni, nj, si, sj, ei, ej, done);
+			if (done)
+				return;
+		}
+	}
+	if (!(i == si && j == sj))
+		col[i][j] = 2;
+	render(dfsSleepingTime);
+}
+
 void bfs(int si, int sj, int ei, int ej)
 {
 	queue<pair<int, int>> q;
@@ -209,7 +249,7 @@ void bfs(int si, int sj, int ei, int ej)
 		if (!((f.first == si && f.second == sj) || (f.first == ei && f.second == ej)))
 		{
 			col[f.first][f.second] = 1;
-			render(5);
+			render(bfsSleepingTime);
 		}
 		if (f.first == ei && f.second == ej)
 		{
@@ -217,8 +257,6 @@ void bfs(int si, int sj, int ei, int ej)
 			backTrack(ei, ej, false);
 			return;
 		}
-		srand(time(NULL));
-		random_shuffle(dir.begin(), dir.end());
 		for (int z = 0; z < 8; ++z)
 		{
 			int ni = f.first + dir[z].first;
@@ -232,7 +270,7 @@ void bfs(int si, int sj, int ei, int ej)
 		}
 	}
 	clear();
-	render(5);
+	render(bfsSleepingTime);
 }
 
 void bibfs(int si, int sj, int ei, int ej)
@@ -251,9 +289,7 @@ void bibfs(int si, int sj, int ei, int ej)
 			col[f1.first][f1.second] = 1;
 		if (!((f2.first == si && f2.second == sj) || (f2.first == ei && f2.second == ej)))
 			col[f2.first][f2.second] = 1;
-		render(6);
-		srand(time(NULL));
-		random_shuffle(dir.begin(), dir.end());
+		render(bibfsSleepingTime);
 		for (int z = 0; z < 8; ++z)
 		{
 			int ni = f1.first + dir[z].first;
@@ -291,13 +327,15 @@ void bibfs(int si, int sj, int ei, int ej)
 
 void bestFirstSearch(int si, int sj, int ei, int ej)
 {
-	openList.push_back({ si, sj });
-	open[si][sj] = true;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp3> openList;
 	h[si][sj] = 0;
-	make_heap(openList.begin(), openList.end(), cmp3);
+	h[ver][hor] = INT_MAX;
+	exists[si][sj] = true;
+	openList.push({ si, sj });
 	while (!openList.empty())
 	{
-		auto p = openList.front();
+		auto p = openList.top();
+		openList.pop();
 		if (p.first == ei && p.second == ej)
 		{
 			clear();
@@ -307,46 +345,37 @@ void bestFirstSearch(int si, int sj, int ei, int ej)
 		if (!((p.first == si && p.second == sj)))
 		{
 			col[p.first][p.second] = 1;
-			render(15);
+			render(bestFirstSearchSleepingTime);
 		}
 		for (int z = 0; z < 8; ++z)
 		{
 			int ni = p.first + dir[z].first;
 			int nj = p.second + dir[z].second;
-			if (ni >= 0 && ni < ver && nj >= 0 && nj < hor && col[ni][nj] != 3 && !open[ni][nj])
+			if (ni >= 0 && ni < ver && nj >= 0 && nj < hor && col[ni][nj] != 3 && !exists[ni][nj])
 			{
-				open[ni][nj] = true;
+				exists[ni][nj] = true;
 				h[ni][nj] = calcH(ni, nj, ei, ej);
 				pi[ni][nj] = p.first;
 				pj[ni][nj] = p.second;
-				openList.push_back({ ni, nj });
+				openList.push({ ni, nj });
 			}
 		}
-		vector<pair<int, int>> nvec;
-		for (int i = 1; i < (int)openList.size(); ++i)
-			nvec.push_back(openList[i]);
-		openList.clear();
-		openList = nvec;
-		make_heap(openList.begin(), openList.end(), cmp3);
+		openList.push({ ver, hor });
 	}
 	clear();
 }
+
 void dijkstra(int si, int sj, int ei, int ej)
 {
-	vector<pair<int, int>> vec;
-	for (int i = 0; i < ver; ++i)
-		for (int j = 0; j < hor; ++j)
-			vec.push_back({ i, j });
+	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
 	dist[si][sj] = 0;
-	make_heap(vec.begin(), vec.end(), cmp);
-	while (!vec.empty())
+	dist[ver][hor] = INT_MAX;
+	exists[si][sj] = true;
+	pq.push({ si, sj });
+	while (!pq.empty())
 	{
-		auto mn = vec.front();
-		if (dist[mn.first][mn.second] == INT_MAX)
-		{
-			clear();
-			return;
-		}
+		auto mn = pq.top();
+		pq.pop();
 		if (mn.first == ei && mn.second == ej)
 		{
 			clear();
@@ -356,36 +385,39 @@ void dijkstra(int si, int sj, int ei, int ej)
 		else if (!(mn.first == si && mn.second == sj))
 			col[mn.first][mn.second] = 1;
 		visited[mn.first][mn.second] = true;
-		render(5);
-		srand(time(NULL));
-		random_shuffle(dir.begin(), dir.end());
+		render(dijkstraSleepingTime);
 		for (int z = 0; z < 8; ++z)
 		{
 			int ni = mn.first + dir[z].first;
 			int nj = mn.second + dir[z].second;
 			if (ni >= 0 && ni < ver && nj >= 0 && nj < hor && col[ni][nj] != 3 && !visited[ni][nj]
 				&& dist[mn.first][mn.second] + 1 < dist[ni][nj])
-				dist[ni][nj] = dist[mn.first][mn.second] + 1, pi[ni][nj] = mn.first, pj[ni][nj] = mn.second;
+			{
+				dist[ni][nj] = dist[mn.first][mn.second] + 1;
+				pi[ni][nj] = mn.first;
+				pj[ni][nj] = mn.second;
+				if (!exists[ni][nj])
+				{
+					pq.push({ ni, nj });
+					exists[ni][nj] = true;
+				}
+			}
 		}
-		vector<pair<int, int>> nvec;
-		for (int i = 1; i < (int)vec.size(); ++i)
-			nvec.push_back(vec[i]);
-		vec.clear();
-		vec = nvec;
-		make_heap(vec.begin(), vec.end(), cmp);
+		pq.push({ ver, hor });
 	}
 }
 
 void aStar(int si, int sj, int ei, int ej)
 {
-	openList.push_back({ si, sj });
-	open[si][sj] = true;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp2> openList;
 	g[si][sj] = h[si][sj] = 0;
-	make_heap(openList.begin(), openList.end(), cmp2);
-	while (openList.size())
+	g[ver][hor] = INT_MAX >> 1, h[ver][hor] = INT_MAX >> 1;
+	exists[si][sj] = true;
+	openList.push({ si, sj });
+	while (!openList.empty())
 	{
-		auto p = openList.front();
-		open[p.first][p.second] = false;
+		auto p = openList.top();
+		openList.pop();
 		if (p.first == ei && p.second == ej)
 		{
 			clear();
@@ -395,7 +427,7 @@ void aStar(int si, int sj, int ei, int ej)
 		if (!(p.first == si && p.second == sj))
 		{
 			col[p.first][p.second] = 1;
-			render(7);
+			render(aStarSleepingTime);
 		}
 		for (int z = 0; z < 8; ++z)
 		{
@@ -407,19 +439,14 @@ void aStar(int si, int sj, int ei, int ej)
 				h[ni][nj] = calcH(ni, nj, ei, ej);
 				pi[ni][nj] = p.first;
 				pj[ni][nj] = p.second;
-				if (!open[ni][nj])
+				if (!exists[ni][nj])
 				{
-					openList.push_back({ ni, nj });
-					open[ni][nj] = true;
+					openList.push({ ni, nj });
+					exists[ni][nj] = true;
 				}
 			}
 		}
-		vector<pair<int, int>> nvec;
-		for (int i = 1; i < (int)openList.size(); ++i)
-			nvec.push_back(openList[i]);
-		openList.clear();
-		openList = nvec;
-		make_heap(openList.begin(), openList.end(), cmp2);
+		openList.push({ ver, hor });
 	}
 	clear();
 }
@@ -452,7 +479,15 @@ int main()
 			else if (!((i == si && j == sj) || (i == ei && j == ej)))
 				col[i][j] = 3;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::B) && ei != -1) // B -> BFS
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) && ei != -1) // D -> DFS
+		{
+			ok = false;
+			bool bol = false;
+			clearYellow();
+			reset(si, sj, ei, ej);
+			dfs(si, sj, si, sj, ei, ej, bol);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::B) && ei != -1) // B -> BFS
 		{
 			ok = false;
 			clearYellow();
